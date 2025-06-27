c600e3d6d265c27a8f42bc986c5a30605ebcb30e
meksbish1 <maheklineswala@gmail.com>
2025-06-28 00:06:31 +0530

    day 1

--- js basics/day 1.js
+++ js basics/day 1.js
@@ -1,8 +1,10 @@
+//task 1
 let name = "pookss"
 let age = 10
 let city = "nyc"
 console.log("Hello, my name is " + name + ", I am " + age + " years old and I live in " + city + ".");
 
+//task 2
 let isstudent = true
 if (isstudent) {
     console.log("I am a student.");
@@ -11,15 +13,18 @@ else{
     console.log("im not A STUDENT");
 } 
 
+//task 3
 const PI = 3.14
 console.log(PI);
 const PI = 4 //error
 
+//task 4
 let hobbies = ["gaming","reading","sleeping"]
 for (let i =0; i<3;i++){
     console.log(hobbies[i]);
 }
 
+//task 5
 let person = {
     name:"pookss ki sardar" ,
     age: 10 ,
