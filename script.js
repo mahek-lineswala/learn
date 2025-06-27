518308230148913c430fd74fff4cf900b634f692
meksbish1 <maheklineswala@gmail.com>
2025-06-28 00:03:46 +0530

    day 1

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const number =10
-const n1=5
+let name = "pookss"
+let age = 10
+let city = "nyc"
+console.log("Hello, my name is " + name + ", I am " + age + " years old and I live in " + city + ".");
+
+let isstudent = true
+if (isstudent) {
+    console.log("I am a student.");
+}  
+else{
+    console.log("im not A STUDENT");
+} 
+
+const PI = 3.14
+console.log(PI);
+const PI = 4 //error
+
+let hobbies = ["gaming","reading","sleeping"]
+for (let i =0; i<3;i++){
+    console.log(hobbies[i]);
+}
+
+let person = {
+    name:"pookss ki sardar" ,
+    age: 10 ,
+    city: "nyc"
+}
